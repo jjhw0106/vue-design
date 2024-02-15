a2e4721ff7f6dd5b1273d66c670e1dc7cd2c777e
jjhw0106 <jjhw0106@jinhak.com>
2024-02-15 09:17:33 +0900

    Feat: controlled

---
+++ controlled/src/components/CheckBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="checkbox" :checked="modelValue" @input="toggleCheckBox">
+</template>
+
+<script>
+export default {
+  props:['modelValue'],
+  methods: {
+    toggleCheckBox() {
+      console.log(this.v)
+      this.$emit('update:modelValue', !this.modelValue)
+    }
+  }
+}
+</script>
