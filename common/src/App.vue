2d1d3d1f0a6d5f5a6271affdf4dda4982e8529e2
jjhw0106 <jjhw0106@jinhak.com>
2024-02-15 09:15:22 +0900

    Feat: common

---
+++ common/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-header :title="appTitle"></app-header>
+    <app-content :items="items" @renew="renewItems"></app-content>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+
+export default {
+  components: { 
+    AppHeader,
+    AppContent
+  },
+
+  data() {
+    return {
+      appTitle: "Common Approach",
+      items : [1,2,3,4]
+    }
+  },
+  methods: {
+    renewItems() {
+      console.log("renewItems!!")
+      this.items = [10,20,30,40]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ common/src/components/AppContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="$emit('renew')">renew items</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+  },
+  // methods: {
+  //   sendRenew() {
+  //     this.$emit('renew');
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
