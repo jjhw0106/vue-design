6b1d97789183f6cc5a515b6a79e1ea16bbadabb6
jjhw0106 <jjhw0106@jinhak.com>
2024-02-15 09:18:35 +0900

    Feat:slots

---
+++ slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <item-view>
+      <template #item-one>{{ items }}</template>
+      <template #item-two>{{ items2 }}</template>
+    </item-view>
+  </div>
+</template>
+
+<script>
+import ItemView from './components/Item.vue';
+export default {
+  components: {
+    ItemView,
+  },
+  data() {
+    return {
+      items: ['아이템 1','아이템 2','아이템 3','아이템 4','아이템 5'],
+      items2: ['뉘리리1', '뉘리리 2'],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ slots/src/components/Item.vue
@@ -0,0 +1,40 @@
+ <template>
+  <div class = "user-container">
+      <div>
+        <i class="fa-sharp fa-solid fa-user"></i>
+      </div>
+      <div class="user-description">
+        <!-- 등록하는 영역에서 작성 -->
+        <slot name="item-one"></slot>
+      <div class="time">
+        <slot name="item-two"></slot>
+        sda
+      </div>
+    </div>
+  </div>
+
+   
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+
+}
+.fa-user {
+  font-size: 2.5rem
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
